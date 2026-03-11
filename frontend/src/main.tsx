9f4adb43eec66bc70be03f658c6a2cb239a499c7
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-03-11 23:46:28 +0530

    added typescript

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
-import store from './store/store.js';
+import App from './App.tsx';
+import store from './store/store.ts';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router";
 
