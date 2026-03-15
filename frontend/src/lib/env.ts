1e4826d2d5ba89f8896bde46f8ce7a3b0f05b33c
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-03-15 18:41:51 +0530

    env config changes in frontend

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -1,5 +1,5 @@
 export const getEnv = (key: string) => {
-  const value = import.meta.env[key] || process.env[key];
+  const value = import.meta.env[key];
   if(!value) throw new Error(`Missing environmental Variable: ${key}`);
   return value;
 };
