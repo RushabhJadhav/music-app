5b7ead9ddb950d82054d96f11137fb4115f20c2b
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-03-29 12:49:59 +0530

    added search results component

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,11 +1,18 @@
 import { UserIcon } from 'lucide-react';
 import SearchBar from '../ui/SearchBar';
+import SearchResults from '../ui/SearchResults';
+import { useAppSelector } from '../../store/hooks';
 
 const Header = () => {
+    const result = useAppSelector((state) => state.song.value);
+
     return (
         <header className="border-b-2 p-4 flex justify-between items-center col-span-12 row-span-1">
             <h2>Boka Tunes</h2>
-            <SearchBar />
+            <div className="relative">
+                <SearchBar />
+                { result && <SearchResults /> }
+            </div>
             <UserIcon className="cursor-pointer select-none" />
         </header>
     )
---
+++ frontend/src/components/ui/SearchResults.tsx
@@ -0,0 +1,9 @@
+const SearchResults = () => {
+  return (
+    <div className="absolute border-2 w-full mt-1 rounded-sm">
+      saldfn
+    </div>
+  )
+};
+
+export default SearchResults;
